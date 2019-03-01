d5ddfebdf0ce00812db140413c9307d5c9921d9d
unknown <pcruiher089@gmail.com>
2019-02-28 22:57:24 -0600

    almost done

--- firstImplementation.cpp
+++ firstImplementation.cpp
@@ -22,11 +22,25 @@
 #define mod 1000000007
 #define MOD(x) ((x%mod)+mod)%mod
 #define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define PI 3.1415926535897
 #define vi vector<int>
 using namespace std;
-double rugosity, tubeDiameter, reynolds;
+
+
+double density, viscosity, rugosity, volumetricFlux, internalTubeDiameter, reynolds, velocity;
+int cuenta = 0;
+ll fastPow(ll a,ll b){
+    ll ret = 1;
+    while(b>0){
+        if(b&1) ret = (ret*a);
+        a=(a*a);
+        b>>=1;
+    }
+    return ret;
+}
+
 double frictionFactor(double f){ 
-    return -2*log(rugosity/(3.7*tubeDiameter) + 2.51/(reynolds*sqrt(f))) - 1/f;
+    return 2.0*log((rugosity/internalTubeDiameter)/(3.7) + 2.51/(reynolds*sqrt(f))) + 1/sqrt(f);
 } 
 
 
@@ -39,8 +53,9 @@ void bisection(double a, double b, double error){
     double c = a; 
     while ((b-a) >= error) { 
         c = (a+b)/2; 
-        cout<<"actual value of c: "<<c<<endl;
-        if (!frictionFactor(c)) break; 
+        if(cuenta == 0)cout<<endl;
+        cout<< cuenta++ <<" iteration of c: "<<c<<endl;
+        if (frictionFactor(c) == 0.0) break; 
         else if (frictionFactor(c)*frictionFactor(a) < 0) 
             b = c; 
         else
@@ -52,15 +67,21 @@ void bisection(double a, double b, double error){
 
 int main(){
 sync;
-double charSpeed, tubeDiameter, kinematicSpeed; 
-cout<<"enter characteristic speed "<<endl;
-cin>>charSpeed;
+//double charSpeed, internalTubeDiameter, kinematicSpeed; 
+cout<<"enter density "<<endl;
+cin>>density;
 cout<<"enter tube diameter "<<endl;
-cin>>tubeDiameter;
-cout<<"enter kinematic speed "<<endl;
-cin>>kinematicSpeed;
+cin>>internalTubeDiameter;
+cout<<"enter dynamic viscosity "<<endl;
+cin>>viscosity;
+cout<<"enter rugosity "<<endl;
+cin>>rugosity;
+cout<<"enter volumetric flux "<<endl;
+cin>>volumetricFlux;
 
-reynolds = charSpeed*tubeDiameter/kinematicSpeed;
+velocity = volumetricFlux/(PI*internalTubeDiameter*internalTubeDiameter/4);
+
+reynolds = internalTubeDiameter*velocity*density/viscosity;
 
 int fluxType; //0 for turbulent, 1 for laminar, 2 for transition
 
@@ -72,17 +93,32 @@ if(reynolds>4000){
     fluxType = 2;
 }
 
-if(fluxType == 1){
-    cout<<"friction factor = " << 64/reynolds<<endl;
-}else if(fluxType == 2){
-    cout<<"friction factor is can't be calculated"<<endl;
-}else{
-    cout<<"flux is turbulent, please inputa value for rugosity ";
-    cin>>rugosity;
-    cout<<"friction factor = ";
-    bisection(-1000,1000,0.0);
-}
- 
+cout<<"Reynolds number is = "<<reynolds<<endl;
+//cout<<"flux type number: "<<fluxType<<endl;
 
+if(fluxType == 1){
+    cout<<"Reynolds number represents a lamitar fluid which friction factor is= " << 64/reynolds<<endl;
+}else if(fluxType == 2){
+    cout<<"Reynolds number of this fluid represents a transition flux which friction factor can't be calculated"<<endl;
+}else if(fluxType == 0){
+    cout<<"Reynolds factor represents a turbulent flux, the friction factor of this flux is ";
+    bisection(0.0008,1,0.01);
+    cout<<endl;
+}
 return 0;
 }
+
+/*
+62.3
+1.5
+0.000839
+0.0004
+15
+
+
+62.3
+1.5
+0.000839
+0.0005
+15
+ */
